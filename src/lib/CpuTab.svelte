0a5fee2d561ee5f3237b754cf0797f67c0d9e16e
gusofftech <gustavo.offtech@gmail.com>
2025-08-25 18:01:06 +0200

    Update CpuTab.svelte

--- src/lib/CpuTab.svelte
+++ src/lib/CpuTab.svelte
@@ -4,9 +4,10 @@
 </script>
 
 <h1 class="text-lg font-bold">Engine</h1>
-<PanelButton buttonImage="assets/cheerpx.svg" clickUrl="https://cheerpx.io/docs" buttonText="Explore CheerpX">
+<PanelButton buttonImage="assets/cyber-core.svg" clickUrl="#" buttonText="Cyber Dojo Engine">
 </PanelButton>
-<p><span class="font-bold">Virtual CPU: </span>{$cpuPercentage}%</p>
-<p>CheerpX is a x86 virtualization engine in WebAssembly</p>
-<p>It can securely run unmodified x86 binaries and libraries in the browser</p>
-<p>Excited about our technology? <a class="underline" href="https://cheerpx.io/docs/getting-started" target="_blank">Start building</a> your projects using <a class="underline" href="https://cheerpx.io/" target="_blank">CheerpX</a> today!</p>
+
+<p><span class="font-bold">Віртуальний CPU: </span>{$cpuPercentage}%</p>
+<p>Ти працюєш у повноцінному Linux середовищі, яке запускається прямо у браузері.</p>
+<p>Тут можна виконувати справжні x86-бінарі, тестувати експлойти і тренувати техніки, не встановлюючи нічого на свій ПК.</p>
+<p><em>Це твій особистий тренажер у кібер-додзьо. Використовуй його для практики та експериментів.</em></p>
